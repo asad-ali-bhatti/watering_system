38d2a9cead689cfffe6f3b007136811e2b762707
Asad Ali Bhatti <asadalibhatti@hotmail.com>
2018-07-12 01:23:30 +0200

    server side email settings

--- app/mailers/notify_mailer.rb
+++ app/mailers/notify_mailer.rb
@@ -1,10 +1,10 @@
 class NotifyMailer < ApplicationMailer
-  default from: 'notifications@example.com'
+  default from: 'asadalibhatti.pro@gmail.com'
 
   def notification_email
     @log = params[:log]
 
-    mail(to: 'example@gmail.com', subject: 'Status of Plant Watering Device')
+    mail(to: 'asadalibhatti.pro@gmail.com', subject: 'Status of Plant Watering Device')
   end
 
 end
